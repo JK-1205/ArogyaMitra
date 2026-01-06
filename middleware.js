85694fdb4889c28ca1414dbcb7e41fcd21ba55fc
Jainil Prajapati <jainilp968@gmail.com>
2026-01-06 17:41:19 +0530

    Add Clerk middleware

---
+++ middleware.js
@@ -0,0 +1,21 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isProtectedRoute = createRouteMatcher([
+  "/doctors(.*)",
+  "/onboarding(.*)",
+  "/doctor(.*)",
+  "/admin(.*)",
+  "/video-call(.*)",
+  "/appointments(.*)",
+  "/voice-assistant(.*)",
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (!auth().userId && isProtectedRoute(req)) {
+    return auth().redirectToSignIn();
+  }
+});
+
+export const config = {
+  matcher: ["/((?!_next).*)"],
+};
